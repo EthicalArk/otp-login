1529322e8784315da9524686f5ac6b7aff1e6bf3
Ayush Raj Khare <ayushrajkhare5@gmail.com>
2019-01-12 22:39:33 +0530

    first commit

---
+++ app/src/main/java/com/mysterybae/otplogin/MainActivity.java
@@ -0,0 +1,123 @@
+package com.mysterybae.otplogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText et1,et2,et3,et4,et5,et6;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1 = findViewById(R.id.editText1);
+        et2 = findViewById(R.id.editText2);
+        et3 = findViewById(R.id.editText3);
+        et4 = findViewById(R.id.editText4);
+
+
+        et1.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length()==1)
+                {
+                    et2.requestFocus();
+                }
+                else if(s.length()==0)
+                {
+                    et1.clearFocus();
+                }
+            }
+        });
+
+        et2.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length()==1)
+                {
+                    et3.requestFocus();
+                }
+                else if(s.length()==0)
+                {
+                    et1.requestFocus();
+                }
+            }
+        });
+
+        et3.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length()==1)
+                {
+                    et4.requestFocus();
+                }
+                else if(s.length()==0)
+                {
+                    et2.requestFocus();
+                }
+            }
+        });
+
+        et4.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                if(s.length()==1)
+                {
+                    et4.requestFocus();
+                }
+                else if(s.length()==0)
+                {
+                    et3.requestFocus();
+                }
+            }
+        });
+
+
+    }
+}
